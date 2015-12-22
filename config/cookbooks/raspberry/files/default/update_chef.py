72d2cfdef9a220b64d5095f89815bd6d73f5601a
Tom Parker <palfrey@tevp.net>
2015-12-22 00:31:22 +0000

    Make script log properly

--- config/cookbooks/raspberry/files/default/update_chef.py
+++ config/cookbooks/raspberry/files/default/update_chef.py
@@ -7,6 +7,21 @@ from os.path import exists
 from os import chdir
 from git import Repo, NoSuchPathError
 import shutil
+import logging
+
+logger = logging.getLogger(__name__)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+ch = logging.FileHandler("/var/log/update-chef.log")
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 def internet_on():
     try:
@@ -28,39 +43,44 @@ while True:
     chef_url = open("/boot/chef-url").read().strip()
     result = urlparse(chef_url)
     if result.scheme == "":
-        print "Bad Chef url in /boot/chef-url: %s" % chef_url
+        logger.warning("Bad Chef url in /boot/chef-url: %s" % chef_url)
         sleep(5)
         continue
 
-    if repo_url != chef_url:
+    if repo_url == chef_url:
+        logger.debug("Repo has correct Chef URL %s" % chef_url)
+    else:
         if exists("/chef_data"): # Existing data, but wrong URL
+            logger.debug("Killing old /chef_data for %s" % repo_url)
             shutil.rmtree("/chef_data")
 
-        print "Trying Chef URL %s" % chef_url
+        logger.info("Trying Chef URL %s" % chef_url)
 
         if internet_on():
             try:
-                subprocess.check_call(["git", "clone", chef_url, "/chef_data"])
-            except subprocess.CalledProcessError:
-                print "error while calling chef, pausing"
+                subprocess.check_output(["git", "clone", chef_url, "/chef_data"])
+            except subprocess.CalledProcessError, e:
+                logger.exception("Error while calling chef, pausing")
+                logger.error(e.output)
                 sleep(5)
                 continue
         else:
-            print "Can't get to the internet, pausing"
+            logger.warning("Can't get to the internet, pausing")
             sleep(5)
             continue
 
     chdir("/chef_data")
     chef_cookbook = open("/boot/chef-cookbook").read().strip()
     if chef_cookbook == "":
-        print "No Chef cookbook specified in /boot/chef-cookbook"
+        logger.warning("No Chef cookbook specified in /boot/chef-cookbook")
         sleep(5)
         continue
     try:
-        subprocess.check_call(["git", "pull"], stdout = subprocess.PIPE)
-        subprocess.check_call(["chef-client", "-z", "-c", "client.rb", "-o", chef_cookbook])
+        subprocess.check_output(["git", "pull"])
+        subprocess.check_output(["chef-client", "-z", "-c", "client.rb", "-o", chef_cookbook])
         sleep(60 * 60) # Waiting an hour, because all ok
-    except subprocess.CalledProcessError:
-        print "error while calling chef, pausing"
+    except subprocess.CalledProcessError, e:
+        logger.exception("Error while calling chef, pausing")
+        logger.error(e.output)
         sleep(60)
         continue
